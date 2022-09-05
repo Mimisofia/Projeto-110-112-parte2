e9183c3d29f723ab44effdf1412ef29a8f6238ff
Mimisofia <99193753+Mimisofia@users.noreply.github.com>
2022-09-04 21:07:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+
+
+Webcam.set({
+    width:350,
+    height:300,
+    imageFormat : 'png' ,
+    pngQuality : 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function takeSnapShot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id = "captured_image" src = "'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version: ', ml5.version);
+
+classifier =  ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Rufp0umpu/model.json',modelLoaded);
+
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = toSpeak;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+}
+ function check()
+ {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+ }
+
+ function gotResult(error, results) 
+ {
+    if(error) {
+        console.error(error);
+    } else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+
+        gesture = results[0].label;
+
+        toSpeak = "";
+        
+        
+        speak();
+        if(gesture == "Legal")
+        {
+            document.getElementById("result_object_gesture_icon").innerHTML = "&#128077;"
+        }
+        else if(gesture == "Tranquilo")
+        {
+            document.getElementById("result_object_gesture_icon").innerHTML = "&#129305;"
+        }
+        else if(gesture == "Vitória")
+        {
+            document.getElementById("result_object_gesture_icon").innerHTML = "&#9996;"
+        }
+        else if(gesture == "Perfeito")
+        {
+            document.getElementById("result_object_gesture_icon").innerHTML = "&#128076;"
+        }
+        else if(gesture == "Olá!")
+        {
+            document.getElementById("result_object_gesture_icon").innerHTML = "&#128075;"
+        }
+        else if(gesture == "Sou do rock")
+        {
+            document.getElementById("result_object_gesture_icon").innerHTML = "&#129311;"
+        }
+        else if(gesture == "Amém")
+        {
+            document.getElementById("result_object_gesture_icon").innerHTML = "&#128591;"
+        }
+        else if(gesture == "Não gostei")
+        {
+            document.getElementById("result_object_gesture_icon").innerHTML = "&#128078;"
+        }
+        
+        
+
+    }
+ }
